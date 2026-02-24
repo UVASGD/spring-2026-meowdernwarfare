1275dc68348e8ac1f7872fec6b12166d531e877f
mikevillanuevanajarro <mikevillanuevanajarro@gmail.com>
2026-02-23 19:20:47 -0500

    Added in the slightest of changes to loanshark, and implemented their sprites

--- scripts/heroes/loanshark.gd
+++ scripts/heroes/loanshark.gd
@@ -1,6 +1,12 @@
 class_name HeroLoanShark
 extends Hero
 
+var meleeDamage = 20 #maybe for a melee character, we'd have a higher base damage 
+var meleeCooldown = 1 #one swing per second
+var paymentPlanCooldown = 7
+var paymentPlanAbilityCount = 2
+var reapoCooldown = 8
+
 func get_hero_name() -> String:
 	return "LoanShark"
 
@@ -12,3 +18,6 @@ func _do_ability1(aim_dir: Vector2, aim_pos: Vector2) -> void:
 
 func _do_ability2(aim_dir: Vector2, aim_pos: Vector2) -> void:
 	print("LoanShark: ability2")
+
+func _do_ultimate(aim_dir: Vector2, aim_pos: Vector2) -> void:
+	print("LoanShark: ultimate")
